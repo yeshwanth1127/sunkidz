332fc864556fe4426e91ebb77e26a8bf9397b5a5
SunderGanesan <85928145+SunderGanesan@users.noreply.github.com>
2026-05-26 18:14:05 +0530

    fix: correct alembic migration chain and deploy learning modules to production

--- backend/alembic/versions/011_add_branch_type.py
+++ backend/alembic/versions/011_add_branch_type.py
@@ -1,7 +1,7 @@
 """add branch_type to branches
 
 Revision ID: 011_add_branch_type
-Revises: 010_daycare_daily_updates
+Revises: 011_notification_sender_id
 Create Date: 2026-04-23
 
 """
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = '011_add_branch_type'
-down_revision = '010_daycare_daily_updates'
+down_revision = '011_notification_sender_id'
 branch_labels = None
 depends_on = None
 
